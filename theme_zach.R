c15e61bbb33ece96396711db36b80b384ac99676
Zachary Richardson <zacrich16@gmail.com>
2025-10-01 14:13:17 -0400

    Split off colors into scale_fill_zach and scale_color_zach
    - Changed color palette to 20 colorblind-friendly colors
    - Removed updating geom defaults due to color issue

--- theme_zach.R
+++ theme_zach.R
@@ -1,18 +1,12 @@
 ## Create custom theme
 theme_zach <- function(base_size = 11, base_family = "sans") {
   
-  ## Create custom color palette for ggplot
-  custom_palette = c(
-    "#1E90FF", "#F28E2B", "#E15759", "#76B7B2", "#59A14F",
-    "#EDC949", "#AF7AA1", "#FF9DA7", "#9C755F", "#BAB0AC"
-  )
-  
   ## Update default shape, size, outline color, and outline thickness for geom_point
   update_geom_defaults("point", list(shape = 21, size = 2, colour = "black", stroke = 0.5))
   
-  ## Update default outline color for geom_bar and geom_col
-  update_geom_defaults("bar", list(colour = "black"))
-  update_geom_defaults("col", list(colour = "black"))
+  # ## Update default outline color for geom_bar and geom_col
+  # update_geom_defaults("bar", list(colour = "black"))
+  # update_geom_defaults("col", list(colour = "black"))
   
   ## Create custom theme
   list(
@@ -87,13 +81,36 @@ theme_zach <- function(base_size = 11, base_family = "sans") {
         # Text
         strip.text = element_text(
           size = rel(0.9), face = "bold", margin = margin(b = 2))
-      ),
-    
-    ## Update colors with custom palette
-    scale_fill_manual(values = custom_palette),
-    
-    scale_color_manual(values = custom_palette)
+      )
     
   )
   
+}
+
+
+
+
+scale_fill_zach <- function(...) {
+  ggplot2::scale_fill_manual(
+    values = c(
+      "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
+      "#D55E00", "#CC79A7", "#999999", "#882255", "#44AA99",
+      "#117733", "#DDCC77", "#332288", "#AA4499", "#6699CC",
+      "#88CCEE", "#44BB99", "#999933", "#CC6677", "#AA4466"
+    ),
+    ...
+  )
+}
+
+
+scale_color_zach <- function(...) {
+  ggplot2::scale_color_manual(
+    values = c(
+      "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
+      "#D55E00", "#CC79A7", "#999999", "#882255", "#44AA99",
+      "#117733", "#DDCC77", "#332288", "#AA4499", "#6699CC",
+      "#88CCEE", "#44BB99", "#999933", "#CC6677", "#AA4466"
+    ),
+    ...
+  )
 }
